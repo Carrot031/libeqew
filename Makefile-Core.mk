4d3d4e4c5aa807d1742957e7c4bd88c2d9f9821a
BuildTools <www.carrotsoft@gmail.com>
2015-08-18 11:40:13 +0900

    Some update on build system.

--- Makefile-Core.mk
+++ Makefile-Core.mk
@@ -1,9 +1,4 @@
-CC=g++
-CFLAGS=-Wall -std=c++11 -g -fPIC -O0
-OUTPUT=libeqew.a
-BUILDDIR = build/$(ARCH)
 OBJS=EQEW.o  $(addsuffix .o,$(basename $(wildcard liboauthcpp/*.cpp))) $(addsuffix .o,$(basename $(wildcard json11/*.cpp)))
-ARCH=$(shell arch)
 #LIBS=liboauthcpp/*.o
 all: $(OUTPUT)
 $(OUTPUT) : $(OBJS) $(LIBS)
